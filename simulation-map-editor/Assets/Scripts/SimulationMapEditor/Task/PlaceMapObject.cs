c15fd17701db5d4bfdf620777614150b1e8cd9a9
山本直樹 <yamamoto.naoki@techcross.co.jp>
2019-07-19 20:18:16 +0900

    テストコード

--- simulation-map-editor/Assets/Scripts/SimulationMapEditor/Task/PlaceMapObject.cs
+++ simulation-map-editor/Assets/Scripts/SimulationMapEditor/Task/PlaceMapObject.cs
@@ -16,6 +16,13 @@ namespace AlterEditor.SimulationMapEditor
             m_Camera = GameObject.Find("Camera").GetComponent<Camera>();
             m_Canvas = GameObject.Find("UICanvas").GetComponent<Canvas>();
             m_Distance = m_Camera.WorldToScreenPoint(GameObject.Find("Object").transform.position).z;
+
+#if false
+            var pos = Vector2.zero;
+            var screenPos = RectTransformUtility.WorldToScreenPoint(m_Camera,)
+            RectTransformUtility.ScreenPointToLocalPointInRectangle();
+            m_GridPos = m_Camera.ViewportToWorldPoint(new Vector3(75f, 75f, m_Distance));
+#endif
         }
 
         public override void Update()
@@ -27,11 +34,30 @@ namespace AlterEditor.SimulationMapEditor
                     : null;
                 if (obj != null)
                 {
+#if true
+                    var gridParent = GameObject.Find("GridParent");
+                    var grid0 = gridParent.transform.GetChild(0).GetComponent<GridBase>().transform.position;
+                    var grid1 = gridParent.transform.GetChild(1).GetComponent<GridBase>().transform.position;
+
+                    var diff = Vector3.Distance(grid0, grid1);
+                    
+                    var mouse = Input.mousePosition;
+                    mouse.z = m_Distance;
+                    mouse = m_Camera.ScreenToWorldPoint(mouse);
+
+                    float x = (int) (mouse.x / diff) * diff;
+                    float y = (int) (mouse.y / diff) * diff;
+                    //obj.transform.position = mouse;
+                    obj.transform.position = new Vector3(x, y, obj.transform.position.z);
+#endif
+
+#if false
                     var pos = Input.mousePosition;
                     pos.z = m_Distance;
                     pos = m_Camera.ScreenToWorldPoint(pos);
                     obj.transform.position = pos;
-               }
+#endif
+                }
             }
         }
 
